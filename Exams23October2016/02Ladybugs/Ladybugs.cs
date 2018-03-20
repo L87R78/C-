a00d081151355e1015ba8e46aa006b8b89423d4e
L87R78 <L87R78@gmail.com>
2018-03-20 15:02:13 +0200

    update

---
+++ Exams23October2016/02Ladybugs/Ladybugs.cs
@@ -0,0 +1,90 @@
+﻿namespace _02Ladybugs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Ladybugs
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var arrBugs = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var newArrBugs = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                newArrBugs.Add(0); 
+            }
+            
+            for (int a = 0; a < size; a++)
+            {
+                for (int b = 0; b < arrBugs.Count; b++)
+                {
+                    if (a == arrBugs[b])
+                    {
+                        newArrBugs.RemoveAt(a);
+                        newArrBugs.Insert(a, 1);
+                        break;
+                    }
+                  
+                }
+            }
+            
+            while (true)
+            {
+                var commandLine = Console.ReadLine().Split();
+                if (commandLine[0] == "end")
+                {
+                    break;
+                }
+
+                int indexBugs = int.Parse(commandLine[0]);
+                string command = commandLine[1];
+                int flylenght = int.Parse(commandLine[2]);
+
+                if (indexBugs < 0 || indexBugs >= size)
+                {
+                    continue;
+                }
+
+                if (newArrBugs[indexBugs] == 0)
+                {
+                    continue;
+                }
+                newArrBugs[indexBugs] = 0;
+                var position = indexBugs;
+                while (true)
+                {
+                    if (command == "right")
+                    {
+                        position += flylenght;
+                    }
+                    else
+                    {
+                        position -= flylenght;
+                    }
+
+                    if (position < 0 || position >= size)
+                    {
+                        break;
+                    }
+
+                    if (newArrBugs[position] == 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        newArrBugs[position] = 1;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ",newArrBugs));
+        }
+    }
+}
---
+++ Exams23October2016/CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,31 @@
+﻿namespace CharityMarathon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            long lenghtMarathonDays = long.Parse(Console.ReadLine());
+            long numberRunners = long.Parse(Console.ReadLine());
+            long averageNumberLapsRunner = long.Parse(Console.ReadLine());
+            long lenghtTrack = long.Parse(Console.ReadLine());
+            long capacityTrack = long.Parse(Console.ReadLine());
+
+            double moneyKilometer = double.Parse(Console.ReadLine());
+
+
+            capacityTrack = capacityTrack * lenghtMarathonDays;
+            numberRunners = Math.Min(capacityTrack, numberRunners);
+
+            long totalMeters = numberRunners * averageNumberLapsRunner * lenghtTrack;
+            long totalKilometers = totalMeters / 1000;
+            double resultMoneyMarathon = totalKilometers * moneyKilometer;
+            Console.WriteLine($"Money raised: {resultMoneyMarathon:F2}");
+        }
+    }
+}
