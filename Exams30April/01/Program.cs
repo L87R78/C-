21c08f2d5ba4a71afc68c83f25c04fe51d528571
L87R78 <L87R78@gmail.com>
2018-03-20 15:04:40 +0200

    update

---
+++ Exams30April/01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            long lenghtBugs = long.Parse(Console.ReadLine());
+            decimal widhtBugs = decimal.Parse(Console.ReadLine());
+
+
+            long lenghtResult = lenghtBugs * 100;
+            decimal result = lenghtResult % widhtBugs;
+
+       
+
+            if (result == 0)
+            {
+                decimal finish = (lenghtBugs * widhtBugs) * 100;
+             
+                Console.WriteLine($"{finish:F2}");
+            }
+            else
+            {
+                decimal finish = (lenghtResult / widhtBugs) * 100;
+                Console.WriteLine($"{finish:F2}%");
+            }
+        }
+    }
+}
---
+++ Exams30April/04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(new char[] { ' ' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+
+            var list2 = list;
+            int count = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                count++;
+                if (list[i] != 0)
+                {
+                    int n = list[i];
+                    list.RemoveAt(i);
+                    list.Insert(i, 0);
+                    i = n;
+                }
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
