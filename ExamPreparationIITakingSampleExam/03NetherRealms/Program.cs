9f6764a52362e2e4a708a0c0cd0b27683faa6419
L87R78 <L87R78@gmail.com>
2018-03-20 14:36:49 +0200

    update

---
+++ ExamPreparationIITakingSampleExam/03NetherRealms/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var text in inputLine.OrderBy(x => x))
+            {
+                long health = 0;
+                StringBuilder temp = new StringBuilder();
+                foreach (char letter in text)
+                {
+                    bool digit = char.IsDigit(letter);
+
+                    if (digit == false && letter != '.' && letter != '*' && letter != '-' && letter != '+' && letter != '/')
+                    {
+                        health += (int)letter;
+                    }
+                    else if (letter == '*' || letter == '/')
+                    {
+                        temp.Append(letter);
+                    }
+
+                }
+                double demage = 0;
+                StringBuilder digitSb = new StringBuilder();
+
+                foreach (var digit in text)
+                {
+                    if (digit >= '0' && digit <= '9' || digit == '-')
+                    {
+                        digitSb.Append(digit);
+                    }
+                    else if (digit == '.')
+                    {
+                        digitSb.Append(digit);
+                    }
+                    else
+                    {
+                        digitSb.Append(" ");
+                    }
+                }
+
+                string result = digitSb.ToString();
+
+                List<string> list = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                foreach (var item in list)
+                {
+                    demage += double.Parse(item);
+                }
+
+                string tempNew = temp.ToString();
+                foreach (var symbol in tempNew)
+                {
+                    if (symbol == '*')
+                    {
+                        demage *= 2;
+                    }
+                    else if (symbol == '/')
+                    {
+                        demage /= 2;
+                    }
+
+                }
+                Console.WriteLine($"{text} - {health} health, {demage:F2} damage");
+            }
+        }
+    }
+}
---
+++ ExamPreparationIITakingSampleExam/04RoliTheCoder/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04RoliTheCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Dictionary<string, string>> dic = new Dictionary<int, Dictionary<string, string>>();
+            while (true)
+            {
+                var inputLIne = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputLIne[0] == "Time")
+                {
+                    break;
+                }
+
+                int ID = int.Parse(inputLIne[0]);
+                string name = inputLIne[1];
+                string participant = "";
+                bool test = true;
+                for (int i = 2; i < inputLIne.Length; i++)
+                {
+                    foreach (var letter in inputLIne[i])
+                    {
+                        string tempLet = letter.ToString();
+                        if (tempLet == "@")
+                        {
+                            participant += inputLIne[i] + " ";
+                            break;
+                        }
+                        else
+                        {
+                            test = false;
+                            break;
+                        }
+                    }
+                    if (test == false)
+                    {
+                        break;
+                    }
+
+                }
+
+                if (test == false)
+                {
+                    continue;
+                }
+
+                foreach (var item in name)
+                {
+                    string temp = item.ToString();
+                    if (temp == "#")
+                    {
+                        if (!dic.ContainsKey(ID))
+                        {
+                            dic.Add(ID, new Dictionary<string, string>());
+                            dic[ID][name] = participant;
+                        }
+                        else
+                        {
+                            if (dic[ID].ContainsKey(name))
+                            {
+                                dic[ID][name] += participant;
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                        break;
+                    }
+                }
+            }
+
+            Dictionary<string, List<string>> dicFinish = new Dictionary<string, List<string>>();
+
+            foreach (var item in dic)
+            {
+                foreach (var ite in item.Value)
+                {
+                    List<string> listTemp = ite.Value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Distinct().ToList();
+
+                    if (!dicFinish.ContainsKey(ite.Key))
+                    {
+                        dicFinish.Add(ite.Key, new List<string>());
+                        foreach (var it in listTemp)
+                        {
+                            dicFinish[ite.Key].Add(it);
+                        }
+
+                    }
+                }
+            }
+
+            Dictionary<string, string> kur = new Dictionary<string, string>();
+            foreach (var item in dicFinish.OrderBy(x => x.Key).OrderByDescending(x => x.Value.Count()))
+            {
+                Console.WriteLine($"{item.Key.Trim('#')} - {item.Value.Count}");
+
+                foreach (var ite in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine(ite);
+                }
+
+            }
+        }
+    }
+}
